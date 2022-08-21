c1bf44cda3ae0a1296a387fe6fb58216c4a97dc4
M4c3j <105147693+M4c3j@users.noreply.github.com>
2022-08-21 19:16:26 +0200

    Initial Commit

---
+++ Planer/ContentView.swift
@@ -0,0 +1,25 @@
+//
+//  ContentView.swift
+//  Planer
+//
+//  Created by Maciej Lipiec on 2022-08-21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundColor(.accentColor)
+            Text("Hello, world!")
+        }
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Planer/PlanerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PlanerApp.swift
+//  Planer
+//
+//  Created by Maciej Lipiec on 2022-08-21.
+//
+
+import SwiftUI
+
+@main
+struct PlanerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
